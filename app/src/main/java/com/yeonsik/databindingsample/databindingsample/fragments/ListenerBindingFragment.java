66d39a87f430af7a1cd975cedce16998e6500e0c
yeonsik <siktaeng@naver.com>
2017-03-04 02:46:34 +0900

    설명 주석 추가

--- app/src/main/java/com/yeonsik/databindingsample/databindingsample/fragments/ListenerBindingFragment.java
+++ app/src/main/java/com/yeonsik/databindingsample/databindingsample/fragments/ListenerBindingFragment.java
@@ -46,10 +46,27 @@ public class ListenerBindingFragment extends BaseFragment {
         Toast.makeText(getContext(), "listenerBindings : User-" + user.getFirstName() + user.getLastName(), Toast.LENGTH_SHORT).show();
     }
 
-    public void completeChanged(View view, boolean isChecked, User user) {
+    /**
+     * 원래의 event의 return 값이 void 라면 다른 return 값으로도 사용할 수 있다.
+     * @param view
+     * @param isChecked
+     * @param user
+     * @return
+     */
+    public boolean completeChanged(View view, boolean isChecked, User user) {
         Toast.makeText(getContext(), "completeChanged : User-" + user.getFirstName() + user.getLastName() + ", " + isChecked, Toast.LENGTH_SHORT).show();
+        return true;
     }
 
+    /**
+     * 원래의 event의 return값이 void가 아닌 경우 원래의 event return 값과 똑같이 맞춰줘야 한다.
+     * setOnLongClickListener 에 구현된 onLongClick(View v) 메서드는 boolean 값을 return 하므로
+     * 이 메서드도 onLongClick 이벤트로 사용되는 경우 boolean 값을 return 해줘야 한다.
+     *
+     * @param view
+     * @param user
+     * @return
+     */
     public boolean onLongClick(View view, User user) {
         Toast.makeText(getContext(), "onLongClick : User-" + user.getFirstName() + user.getLastName(), Toast.LENGTH_SHORT).show();
         return true;
